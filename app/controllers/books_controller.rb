1be94a3a31615b61e9b722b3453ccce4f206cfae
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-03-07 08:04:52 +0000

    [Add]books/index 新規投稿失敗時のエラーメッセージ表示機能（if文）追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,17 +5,19 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books=Book.all
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render (books/index)
+      render :index
     end
   end
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
