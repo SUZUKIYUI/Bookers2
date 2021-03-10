8f77b3e78853f15a25af938fd960235115b5e803
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-03-10 12:46:35 +0000

    [Update]users/editのレイアウト完成・エラーメッセージレイアウト完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,11 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
   end
 
-  def updates
-    user=User.find(params[:id])
-    if user.update(user_params)
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
       flash[:notice]="You have updated user successfully."
-      redirect_to user_path(user.id)
+      redirect_to user_path(@user.id)
     else
       render :edit
     end
